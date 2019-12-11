bdeb80ff750ec02efba34aeb5fd066a5b442c683
hjj <>
2019-12-11 11:44:59 +0800

    update

---
+++ model/base_model.py
@@ -0,0 +1,31 @@
+from peewee import MySQLDatabase, Model, CharField, DateTimeField
+from playhouse.pool import PooledMySQLDatabase
+from Louis.utils import timezone
+from settings import base
+
+
+
+db = PooledMySQLDatabase("louis", host="127.0.0.1", port=3306,
+                         user='root', password='he123456', charset='utf8mb4',
+                         max_connections=4)
+
+
+
+class BaseModel(Model):
+    uid = CharField(verbose_name='ID',max_length=24,index=True,unique=True,primary_key=True)
+    create_user_id = CharField(verbose_name='创建用户id',max_length=24,index=True)
+    create_time = DateTimeField(verbose_name='创建时间')
+    update_user_id = CharField(verbose_name='更新用户id',max_length=24,index=True)
+    update_time = DateTimeField(verbose_name='更新时间')
+    deleted = CharField(verbose_name='已删除',max_length=1,default='0')
+
+    class Meta:
+        database = db
+
+    def save_with_log(self,user_id):
+        if not self.create_user_id:
+            self.create_user_id = user_id
+        self.update_user_id = user_id
+        if not self.create_time:
+            self.create_time = timezone.now()
+        self.save()
